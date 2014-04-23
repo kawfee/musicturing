c7e593d71dc82a75d7978660dfa72617612e240b
Ryan Klimt <ryanklimt@bitbucket.org>
2014-04-23 11:02:11 -0400

    Chrome bug fix

--- audio.php
+++ audio.php
@@ -1,12 +1,14 @@
 <?php
 	session_start();
 	$filename = $_SESSION['data'][$_GET['id']][0] . '.mp3';
+	$filename = '007607b_.mp3';
+	header('Content-Type: audio/mpeg');
+	header('Cache-Control: no-cache');
+	header('Accept-Ranges: bytes');
+	header("Content-Transfer-Encoding: binary");
 	if(file_exists($filename)) {
-		header('Content-Type: audio/mpeg');
-		header('Content-Disposition: filename="'.$filename.'"');
-		header('Content-length: '.filesize($filename));
-		header('Cache-Control: no-cache');
-		header("Content-Transfer-Encoding: chunked"); 
+		header('Content-Disposition: inline; filename="'.$filename.'"');
+		header('Content-Length: '.filesize($filename));
 		readfile($filename);
 	} else {
 		header("HTTP/1.0 404 Not Found");
